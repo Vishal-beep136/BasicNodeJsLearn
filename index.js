9553f13da837dc57f532a2f69a23f381ede498b3
Vishal Beep <alaphtech@gmail.com>
2022-03-19 15:14:37 +0530

    initial-commit

--- index.js
+++ index.js
@@ -188,28 +188,28 @@
 // <-------------------- Search Api in Node Js with MongoDb 🍁🍀 ----------------------->
 
 
-// import express from "express";
-// import connectDb from "./config.js";
-// import product from "./product.js";
+import express from "express";
+import connectDb from "./config.js";
+import product from "./product.js";
 
-// connectDb();
-// const app = express();
-// app.use(express.json());
+connectDb();
+const app = express();
+app.use(express.json());
 
-// app.get("/search/:key", async (req, res) => {
-//     console.log(req.params.key);
-//     let data = await product.find(
-//         {
-//             "$or": [
-//                 { "name": { $regex: req.params.key } },
-//                 { "brand": { $regex: req.params.key } }
-//             ]
-//         }
-//     );
-//     res.send(data);
-// });
+app.get("/search/:key", async (req, res) => {
+    console.log(req.params.key);
+    let data = await product.find(
+        {
+            "$or": [
+                { "name": { $regex: req.params.key } },
+                { "brand": { $regex: req.params.key } }
+            ]
+        }
+    );
+    res.send(data);
+});
 
-// app.listen(1818);
+app.listen(1818);
 
 
 // ---------- `Multer` Upload File Nodejs 👆 ⬆ -------------
